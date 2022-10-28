713ad788ff0eab0ea092b209f7fbe45129a41d35
Spirevipp <bjornar.helgeland@hotmail.com>
2022-10-28 17:25:17 +0200

    :adhesive_bandage:fikset bug i profil

--- profil.js
+++ profil.js
@@ -9,7 +9,7 @@ const username = localStorage.getItem("Username");
 const medlemskap = localStorage.getItem("medlemskap");
 document.getElementById("page-title").firstElementChild.innerHTML =
   "Profil - " + username[0].toUpperCase() + username.substring(1);
-document.getElementById("navn").innerText =
+document.getElementById("navn").innerHTML =
   "Brukernavn: " + username[0].toUpperCase() + username.substring(1);
-document.getElementById("medlemskap").innerText =
+document.getElementById("medlemskap").innerHTML =
   "Aktivt Medlemskap: " + medlemskap[0].toUpperCase() + medlemskap.substring(1);
